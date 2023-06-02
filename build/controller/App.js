3408f1f97f25da2ff4923e7838fd8d0a8ef85b9b
KwakYounHo <kwakyh951@gmail.com>
2023-06-02 14:57:52 +0900

    새로 컴파일링(서버파일)

---
+++ build/controller/App.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, "..", "..", "dist", "index.html"));
+});
+app.use(express_1.default.static(path_1.default.join(__dirname, "..", "..", "dist")));
+app.listen(8080, () => {
+    console.log('8080포트에서 서버 실행 중');
+});
