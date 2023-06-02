ebb7eef63ee0192b197ad88a38a33fadf7695f7b
KwakYounHo <kwakyh951@gmail.com>
2023-06-02 14:39:15 +0900

    서버 컨트롤러 생성

---
+++ controller/App.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import path from 'path';
+
+const app: express.Express = express();
+
+app.get('/', (req: express.Request,res: express.Response) => {
+  res.sendFile(path.join(__dirname, "..", "bundle", "index.html"));
+});
+
+app.use(express.static(path.join(__dirname, "..", "bundle")));
+
+app.listen(8080, ()=>{
+  console.log('8080포트에서 서버 실행 중');
+})
